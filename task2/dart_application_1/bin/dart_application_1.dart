403388c96769ea69f71acb09ffbb40f43fff0251
zainamohaidat <zainamohidat@gmail.com>
2024-07-03 18:10:41 +0300

    Add files via upload

---
+++ task2/dart_application_1/bin/dart_application_1.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+  print('Hello in this code we will using "Escape character"');
+  print("This is backslasg \\");
+  print("This is double qpoutes\"");
+  print("This is single qoutes\'\,");
+  print('we can print new line using \"\\n"');
+  print('and print spaces using \"\\t"');
+}
