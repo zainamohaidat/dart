31bb0a8c90e3e75444f6cf9c6b06a1fa00440c8d
zainamohaidat <zainamohidat@gmail.com>
2024-07-22 16:40:08 +0300

    Delete task8/dart_application_1 directory

--- task8/dart_application_1/bin/dart_application_1.dart
+++
@@ -1,9 +0,0 @@
-class Person {
-  String? _lastName = "zaina";
-  String getLastName() => "$_lastName (Employee)";
-}
-
-void main(List<String> args) {
-  Person p = Person();
-  print(p.getLastName());
-}
